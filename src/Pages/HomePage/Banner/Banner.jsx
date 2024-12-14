8768b7cf25e6f336575c19a7f82d81e52f5da4c5
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-14 12:29:51 +0600

    created banner

---
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Banner = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter" && searchValue.trim()) {
+      console.log(searchValue);
+      setSearchValue("");
+    }
+  };
+  return (
+    <div className="flex flex-col justify-center items-center mb-[8%]">
+      <p className="text-5xl text-slate-700 font-bold mt-[5%] mb-[3%]">
+        Hassle-Free Appointments, Anytime, Anywhere.
+      </p>
+      <p className="w-[60%]  text-slate-400 font-semibold text-center leading-8 mb-[2%]">
+        Take control of your health with our doctor appointment booking app.
+        Find top-rated doctors, check availability, and schedule visits all in
+        one place, making healthcare more accessible and convenient than ever
+        before.
+      </p>
+
+      {/* banner button  */}
+
+      <div className="flex ">
+        <input
+          className="py-2 px-4 w-[400px] focus:outline-none border border-gray-400 rounded-sm"
+          type="text "
+          placeholder="Search your doctors..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyDown={handleSearch}
+        ></input>
+        <button
+          className="bg-green-600 hover:bg-green-700 font-semibold text-lg text-white px-7 py-[10px] rounded-sm ml-5"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
