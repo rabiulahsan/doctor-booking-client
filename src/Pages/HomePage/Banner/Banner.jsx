5b8f04de52ccb98592d0f7c0379a0860de28e31a
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-14 13:15:34 +0600

    banner section animated

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import SvgLine from "../../../Components/SvgLine/SvgLine";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 const Banner = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -10,12 +12,29 @@ const Banner = () => {
       setSearchValue("");
     }
   };
+
+  useGSAP(() => {
+    let bannerTimeline = gsap.timeline();
+    bannerTimeline.from(".banner-title", {
+      y: -100,
+      opacity: 0,
+      delay: 0.4,
+      duration: 0.6,
+    });
+    bannerTimeline.from(".banner-desc, .search-input", {
+      y: 100,
+      opacity: 0,
+      delay: 0.4,
+      duration: 0.6,
+    });
+  });
+
   return (
     <div className="flex flex-col justify-center items-center mb-[8%]">
-      <p className="text-5xl text-slate-700 font-bold mt-[5%] mb-[3%]">
+      <p className="banner-title text-5xl text-slate-700 font-bold mt-[5%] mb-[3%]">
         Hassle-Free Appointments, Anytime, Anywhere.
       </p>
-      <p className="w-[60%]  text-slate-400 font-semibold text-center leading-8 mb-[2%]">
+      <p className="banner-desc w-[60%]  text-slate-400 font-semibold text-center leading-8 mb-[2%]">
         Take control of your health with our doctor appointment booking app.
         Find top-rated doctors, check availability, and schedule visits all in
         one place, making healthcare more accessible and convenient than ever
@@ -24,7 +43,7 @@ const Banner = () => {
 
       {/* banner button  */}
 
-      <div className="flex ">
+      <div className="flex search-input">
         <input
           className="py-2 px-4 w-[400px] focus:outline-none border border-gray-400 rounded-sm"
           type="text "
