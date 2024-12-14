c78e981ef1147a9ef532c0dc2f00d19ea2d88285
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-14 13:29:26 +0600

    category section animated

--- src/Pages/HomePage/Categories/CategoriesCard.jsx
+++ src/Pages/HomePage/Categories/CategoriesCard.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable react/prop-types */
-
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 import { Link } from "react-router-dom";
 
 const CategoriesCard = ({ specialist }) => {
   const { name, data, image } = specialist;
   console.log(specialist);
+
+  useGSAP(() => {
+    gsap.from(".cat-image", {
+      opacity: 0,
+      x: -300,
+      duration: 0.6,
+    });
+    gsap.from(".cat-desc", {
+      opacity: 0,
+      x: 300,
+      duration: 0.6,
+    });
+  });
   return (
     <div className="relative ">
       <div className=" flex justify-between items-center p-5 px-7">
-        <div className="w-1/2 ">
+        <div className="cat-image w-1/2 ">
           <img
             src={image}
             alt="name"
@@ -21,7 +35,7 @@ const CategoriesCard = ({ specialist }) => {
             }}
           />
         </div>
-        <div className="w-1/2 text-slate-600 leading-8 p-4">
+        <div className="cat-desc w-1/2 text-slate-600 leading-8 p-4">
           <p className="font-bold text-2xl  mb-4">{name}</p>
           <p className="">{data}</p>
           <button className=" bg-slate-700 font-semibold  rounded px-5 py-2 text-white mt-5">
