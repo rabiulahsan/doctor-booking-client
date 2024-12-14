a14eaeec89d886a6ac855286db62dedda866ba4c
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-15 00:03:26 +0600

    created search bar

--- src/Pages/DoctorsPage/DoctorsPage.jsx
+++ src/Pages/DoctorsPage/DoctorsPage.jsx
@@ -3,6 +3,15 @@ import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 
 const DoctorsPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log("Searching for:", searchInput);
+  };
 
   const categories = [
     "Cardiologists",
@@ -19,17 +28,35 @@ const DoctorsPage = () => {
   return (
     <div>
       <SectionTitle heading={"Find Your Doctor"}></SectionTitle>
-      <div className="">
-        <div className=""></div>
+      <div className=" flex items-center gap-x-5">
+        <div className="">
+          {/* Search Icon and Input */}
+
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchInput}
+            onChange={handleInputChange}
+            className="outline-none text-slate-700 border border-slate-300 rounded-sm  shadow-sm py-3 px-4 "
+          />
+
+          {/* Search Button */}
+          <button
+            onClick={handleSearch}
+            className=" text-white font-semibold px-5 py-3 ml-5 bg-green-600 hover:bg-green-700 transition rounded-sm"
+          >
+            Search
+          </button>
+        </div>
         <div className="category-selector">
           <select
             id="category"
             value={selectedCategory}
             onChange={handleCategoryChange}
-            className="border border-slate-300  bg-transparent rounded px-5 py-3 text-slate-600 font-semibold"
+            className="border border-slate-300  bg-transparent rounded-sm px-5 py-3 text-slate-700 font-semibold"
           >
             <option className="text-slate-500 " value="" disabled>
-              Choose a category
+              Choose a category{" "}
             </option>
             {categories.map((category, index) => (
               <option
